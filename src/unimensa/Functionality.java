c78b4f7ff48b284dc8237c77ff04c1dd595046e2
FraTheVet <f.vettoretti@gmail.com>
2016-01-04 19:41:05 +0100

    New Comments

--- src/unimensa/Functionality.java
+++ src/unimensa/Functionality.java
@@ -1,5 +1,23 @@
 package unimensa;
 
 public class Functionality {
-
+	/*
+	 * add, remove, read queries
+	 * gotta find a smart way for the signatures
+	 * since we have different number of attributes.
+	 * A solution could be a wise usage of arrays (see signatures)
+	 * but we pass all strings, which sucks
+	 */
+	//just an idea for the signature
+	public void add(String tableName, String[] attributes){
+		
+	}
+	//pretty sure it's enough to have the key and the table to remove a query
+	public void remove(String tableName, int key){
+		
+	}
+	public String[] read(String tableName){
+		String[] 
+		return 
+	}
 }
--- src/unimensa/MainWindow.java
+++ src/unimensa/MainWindow.java
@@ -5,6 +5,9 @@ public class MainWindow {
 	 * main class, creates window with all sub-windows
 	 * no functionalities like add data, remove data, etc...
 	 * respect indentation o ve ammazzo scuoiati
+	 * let's take the standard to comment what we add
+	 * and allert everyone when you add a class and why pls
+	 * <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3 <3
 	*/
 	public static void main(String[] args){
 		
